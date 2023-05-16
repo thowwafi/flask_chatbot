67e8c305e3c9ef2e2b886544b8eb95c2cf13820f
Thowwafi Alfiansyah <thowwafi@gmail.com>
2023-05-16 11:07:04 +0700

    update source path

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.markdown('---')
 st.header('Vector Index')
 st.markdown('Click the button below to rebuild the vector index.')
 if st.button('Rebuild Vector Index'):
-    os.system('python3 ../createVectorIndex.py Store')
+    os.system('python3 createVectorIndex.py Store')
     st.info('Vector Index Rebuilded.')
 # Add data
 st.header('Add Data')
@@ -20,22 +20,22 @@ files = st.file_uploader('Upload file', type=['txt'], accept_multiple_files=True
 for file in files:
     if file:
         # save file
-        with open(f'../Source/{file.name}', 'w') as f:
+        with open(f'Source/{file.name}', 'w') as f:
             f.write(file.getvalue().decode('utf-8'))
         st.info(f'{file.name} saved.')
 # Load data
 st.header('Load Data')
 # read list of files inside Source folder
-files = os.listdir('../Source')
+files = os.listdir('Source')
 # show list of files
 if not files:
     st.info('No file found.')
 for file in files:
     with st.expander(file):
         if st.button('Delete', key=file):
-            os.remove(f'../Source/{file}')
+            os.remove(f'Source/{file}')
             st.info(f'{file} deleted.')
             # check if file exist
-        if os.path.isfile(f'../Source/{file}'):
-            with open(f'../Source/{file}', 'r') as f:
+        if os.path.isfile(f'Source/{file}'):
+            with open(f'Source/{file}', 'r') as f:
                 st.info(f.read())
