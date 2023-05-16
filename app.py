89952024a31cdaffd9bbd708438eb7dfe5198da6
Thowwafi Alfiansyah <thowwafi@gmail.com>
2023-05-16 11:13:36 +0700

    remove mongodb

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 import tiktoken
 from createVectorIndex import model_name
 from decouple import config
-from pymongo import MongoClient
+# from pymongo import MongoClient
 
 
 app = Flask(__name__)
@@ -14,9 +14,9 @@ app.config["DEBUG"] = True
 
 
 os.environ["OPENAI_API_KEY"] = config('OPENAI_API_KEY')
-client = MongoClient('mongodb://localhost:27017/')
-db = client[config('DATABASE_NAME')]
-collection = db[config('DATABASE_COLLECTION_NAME')]
+# client = MongoClient('mongodb://localhost:27017/')
+# db = client[config('DATABASE_NAME')]
+# collection = db[config('DATABASE_COLLECTION_NAME')]
 
 
 @app.route('/')
@@ -60,41 +60,41 @@ def chatbot():
     return jsonify(response)
 
 
-@app.route('/api/chatbot/save_question', methods=['POST'])
-def save_question():
-    """
-    @Body: 
-    {
-        "number": "+6280989999",
-        "question": "apa itu deltacloud.id?"
-    }
-    """
-    request_data = request.json
-    number = request_data.get('number')
-    question = request_data.get('question')
-    # Count token
-    encoding = tiktoken.encoding_for_model(model_name)
-    question_encoded_text = encoding.encode(question)
-    question_token_count = len(question_encoded_text)
+# @app.route('/api/chatbot/save_question', methods=['POST'])
+# def save_question():
+#     """
+#     @Body: 
+#     {
+#         "number": "+6280989999",
+#         "question": "apa itu deltacloud.id?"
+#     }
+#     """
+#     request_data = request.json
+#     number = request_data.get('number')
+#     question = request_data.get('question')
+#     # Count token
+#     encoding = tiktoken.encoding_for_model(model_name)
+#     question_encoded_text = encoding.encode(question)
+#     question_token_count = len(question_encoded_text)
 
-    # save to database
-    record = {
-        "number": number,
-        "question": question,
-        "answer": None,
-        "question_token_count": question_token_count,
-        "answer_token_count": 0,
-        "is_sent": False
-    }
-    collection.insert_one(record)
+#     # save to database
+#     record = {
+#         "number": number,
+#         "question": question,
+#         "answer": None,
+#         "question_token_count": question_token_count,
+#         "answer_token_count": 0,
+#         "is_sent": False
+#     }
+#     collection.insert_one(record)
 
-    response = {}
-    response['status'] = 'Success'
-    response['number'] = number
-    response['question'] = question
-    response['question_token_count'] = question_token_count
+#     response = {}
+#     response['status'] = 'Success'
+#     response['number'] = number
+#     response['question'] = question
+#     response['question_token_count'] = question_token_count
     
-    return jsonify(response)
+#     return jsonify(response)
 
 
 if __name__ == '__main__':
